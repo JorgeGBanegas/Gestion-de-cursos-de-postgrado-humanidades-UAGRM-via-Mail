7fc7147562c86834d855a7de633c118d2efb609c
Jorge Gustavo Banegas <jorge.g.banegas@gmail.com>
2022-10-27 12:29:15 -0400

    commit inicial

---
+++ src/proyectoviamail/ProyectoViaMail.java
@@ -0,0 +1,13 @@
+package proyectoviamail;
+
+/**
+ *
+ * @author Jorge Banegas
+ */
+public class ProyectoViaMail {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
